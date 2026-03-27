7a3c1a30f85f597469eb9076d902ac3db765b281
nikhildotwibe <nikhildotwibe@outlook.com>
2026-03-27 10:04:39 +0530

    up

--- Modules/Settings/Http/Controllers/ActivityTypeController.php
+++ Modules/Settings/Http/Controllers/ActivityTypeController.php
@@ -4,6 +4,8 @@ namespace Modules\Settings\Http\Controllers;
 
 use App\Http\Controllers\BaseController;
 use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Modules\Settings\Entities\ActivityType;
 
 class ActivityTypeController extends BaseController
@@ -13,6 +15,46 @@ class ActivityTypeController extends BaseController
         try {
             $types = ActivityType::all();
             return $this->sendResponse($types, 'Activity Types Fetched', 200);
+        } catch (Exception $exception) {
+            return $this->HandleException($exception);
+        }
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            Validator::make($request->all(), [
+                'name' => 'required|string|max:255',
+            ])->validate();
+
+            $type = ActivityType::create(['name' => $request->name]);
+            return $this->sendResponse($type, 'Activity Type Created', 201);
+        } catch (Exception $exception) {
+            return $this->HandleException($exception);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        try {
+            Validator::make($request->all(), [
+                'name' => 'required|string|max:255',
+            ])->validate();
+
+            $type = ActivityType::findOrFail($id);
+            $type->name = $request->name;
+            $type->save();
+            return $this->sendResponse($type, 'Activity Type Updated', 200);
+        } catch (Exception $exception) {
+            return $this->HandleException($exception);
+        }
+    }
+
+    public function destroy($id)
+    {
+        try {
+            ActivityType::findOrFail($id)->delete();
+            return $this->sendResponse([], 'Activity Type Deleted', 200);
         } catch (Exception $exception) {
             return $this->HandleException($exception);
         }
