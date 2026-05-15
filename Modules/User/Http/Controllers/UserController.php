e51f55444d7cc8ed1e4416efaad01b47f140e4fa
akhil-vj <akhilvj.creator@gmail.com>
2026-05-15 17:30:36 +0530

    undo

--- Modules/User/Http/Controllers/UserController.php
+++ Modules/User/Http/Controllers/UserController.php
@@ -287,11 +287,11 @@ class UserController extends BaseController
                 'c_password' => 'Confirm Password'
             ])->validate();
 
-            $user_roles = Auth::user()->roles->pluck('name');
-            if ($user_roles->isEmpty()) {
+            $roles = Auth::user()->roles;
+            if (!isset($roles[0])) {
                 return $this->sendError('error.', ['error' => 'Logged in user has no valid roles set, so this operation can not be perfomed. '], 401);
             } else {
-                if ($user_roles->contains('Super Admin')) {
+                if ($roles[0]->name == 'Super Admin') {
                     Validator::make($request->all(), [
                         'username' => 'required',
                     ])->validate();
